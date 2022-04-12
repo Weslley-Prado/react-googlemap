209a59e776c2abcec77ad679dad80337f9e1accf
Weslley Prado <weslley.prado73@gmail.com>
2022-04-12 11:55:42 -0300

    Creation of scrutured of project

---
+++ reactmap/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MapReact from './pages/MapReact';
+
+function App() {
+  return (
+    <MapReact />
+  );
+}
+
+export default App;
---
+++ reactmap/src/pages/MapReact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export interface MapReactProps{}
+
+const MapReact =() => {
+    return <div>
+               <p>Bom dia</p>
+    </div>;
+}
+
+export default MapReact;
