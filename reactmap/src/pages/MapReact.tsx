9912034693c5e82cf30eac2171bd89d84bf19d3d
Weslley Prado <weslley.prado73@gmail.com>
2022-04-12 12:46:38 -0300

    Config map

--- reactmap/src/pages/MapReact.tsx
+++ reactmap/src/pages/MapReact.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import React,{ Component } from 'react';
 
 
 export interface MapReactProps{}
 
-const MapReact =() => {
-    return <div>
-               <p>Bom dia</p>
-    </div>;
-}
 
-export default MapReact;
+
+
+class MapReact extends Component {
+    static defaultProps = {
+      center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11
+    };
+  
+    render() {
+      return (
+        // Important! Always set the container height explicitly
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact 
+            bootstrapURLKeys={{ key: 'AIzaSyAD4K4vZN0IagbESNGPdGlap_LWdLYQUOE',}}
+            defaultCenter={{ lat: -15.826691,lng: -47.921822   }}
+            defaultZoom={10}
+          >
+           
+          </GoogleMapReact>
+        </div>
+      );
+    }
+  }
+  
+  export default MapReact;
